f10af84327d6d657277487ba6e1c124bfbd1be27
dangkh0a <149858139+dangkhoa2004@users.noreply.github.com>
2025-05-27 19:04:30 +0700

    Update WebConfig.java

--- pos-backend/src/main/java/com/pos/backend/config/WebConfig.java
+++ pos-backend/src/main/java/com/pos/backend/config/WebConfig.java
@@ -7,7 +7,6 @@ package com.pos.backend.config;
 /**
  * @author 04dkh
  */
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -17,8 +16,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // Áp dụng cho tất cả các API endpoint
-                .allowedOrigins("http://localhost:5173", "http://localhost:8081") // Thay đổi port của Vue.js dev server nếu khác
+        registry.addMapping("/api/**") // Áp dụng cho tất cả các API endpoint bắt đầu bằng /api/
+                // Thay đổi các origins này để phản ánh địa chỉ IP và cổng của frontend của bạn
+                .allowedOrigins("http://192.168.1.7:5173", "http://192.168.1.7:8081")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
