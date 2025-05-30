834253a06c894e41e812a14e11127102c20f4d0d
dangkh0a <149858139+dangkhoa2004@users.noreply.github.com>
2025-05-30 21:47:13 +0700

    [BACKEND] & [FRONTEND] Update Templates

--- pos-frontend/src/App.vue
+++ pos-frontend/src/App.vue
@@ -1,46 +1,12 @@
 <template>
-  <CustomLoading v-if="loadingStore.isLoading"/>
-  <div id="app" class="antialiased text-gray-700 flex flex-col min-h-screen">
-
-    <AppNavbar/>
-
-    <main class="flex-grow container mx-auto pl-4 pr-4">
-      <router-view/>
-    </main>
-
-    <AppFooter/>
-  </div>
+  <ThemeProvider>
+    <SidebarProvider>
+      <RouterView />
+    </SidebarProvider>
+  </ThemeProvider>
 </template>
 
-<script>
-import CustomLoading from "@/components/specific/CustomLoading.vue";
-import {useLoadingStore} from "@/utils/helpers";
-import AppNavbar from './components/layout/AppNavbar.vue';
-import AppFooter from './components/layout/AppFooter.vue';
-
-export default {
-  name: 'App',
-  components: {
-    CustomLoading,
-    AppNavbar,
-    AppFooter,
-  },
-  data() {
-    return {
-      loadingStore: useLoadingStore(),
-    };
-  },
-};
+<script setup>
+import ThemeProvider from './components/layout/ThemeProvider.vue'
+import SidebarProvider from './components/layout/SidebarProvider.vue'
 </script>
-
-<style>
-#app {
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-}
-
-main {
-  flex-grow: 1;
-}
-</style>
