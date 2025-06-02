a1032e0e2b6e9676cff4db49a49959a68a0e815c
dangkh0a <149858139+dangkhoa2004@users.noreply.github.com>
2025-06-02 13:51:22 +0700

    [BACKEND] & [FRONTEND] Update Templates Products / Categorys / Discounts / Invoices

--- pos-frontend/vite.config.ts
+++ pos-frontend/vite.config.ts
@@ -3,12 +3,50 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        "name": "Tên App",
+        "short_name": "Tên Ngắn",
+        "start_url": ".",
+        "display": "standalone",
+        "background_color": "#ffffff",
+        "theme_color": "#6200ee",
+        "icons": [
+          {
+            "src": "/images/logo/192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/images/logo/logo.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ],
+        "screenshots": [
+          {
+            "src": "/images/logo/1080x1920.png",
+            "sizes": "1080x1920",
+            "type": "image/png",
+            "form_factor": "narrow"
+          },
+          {
+            "src": "/images/logo/1920x1080.png",
+            "sizes": "1920x1080",
+            "type": "image/png",
+            "form_factor": "wide"
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
