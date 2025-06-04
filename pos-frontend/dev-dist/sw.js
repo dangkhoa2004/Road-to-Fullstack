c93b1fd6ae35073ea1411ced881a5b0cc5e8b415
dangkh0a <149858139+dangkhoa2004@users.noreply.github.com>
2025-06-04 11:27:47 +0700

    [BACKEND] & [FRONTEND] Update PWA

--- pos-frontend/dev-dist/sw.js
+++ pos-frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.coogf32gkqo"
+    "revision": "0.eg5866rd01"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
